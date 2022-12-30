4fc5021229f960b547de8d0592d327d344a55dd4
Filip Szydłak <266912@student.pwr.edu.pl>
2022-12-30 23:59:07 +0100

    Mycontroller + spring context

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController"); // hey spring, provide me the instance of MyController from the context
+
+        String greeting = myController.sayHello();
+
+        System.out.println(greeting);
+    }
+
+}
