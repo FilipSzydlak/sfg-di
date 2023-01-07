4887e7ca9e5b263e5e1fcd6ec26f4b72a876682c
Filip Szydłak <266912@student.pwr.edu.pl>
2023-01-07 22:55:20 +0100

    Profile and Primary annotation utilized in java based configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,14 +1,30 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
 
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18SpanishGreetingService i18SpanishGreetingService(){
+        return new I18SpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
     @Bean
     ConstructorGreetingService constructorGreetingService(){
         return new ConstructorGreetingService();
